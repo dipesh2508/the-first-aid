c345f434fbfa6bdd8899e48e37577cc5480e8484
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-04-13 01:16:01 +0530

    feat:added sign in/up pages

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
 
 export const config = {
+  publicRoutes: ["/api/webhooks(.*)"],
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
