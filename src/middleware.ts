cf696ddc110ff4e9d569409e68d690ceea9709c0
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-04-14 03:25:18 +0530

    fix:middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/webhooks(.*)", "/"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
   publicRoutes: ["/api/webhooks(.*)", "/"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
   matcher: ["/((?!.+.[w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
